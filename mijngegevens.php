92cc99ad91d84b4d38e56ee3a64080282085a0e5
unknown <you@example.com>
2017-12-03 00:00:17 +0100

    bezig met sql delete

--- mijngegevens.php
+++ mijngegevens.php
@@ -2,11 +2,30 @@
 include 'General.php';
 echo showHeader();
 
+if ($_SESSION['loggedin'] != true) {
+       echo 'not logged in';
+       header("Location: login.php");
+       
+   }
+    
+    $user=$_SESSION['username'];
+?>
 
 
+<head>
 
+        <script>
 
+           document.getElementById("delete").innerHTML = 5 + 6;
 
+        </script>
+
+    </head>
+
+    
+    <button type="button" onclick="delbtn()" id="del">Verwijder mijn gegevens</button>
+
+<?php
     $query= "SELECT * FROM `tjk_users` WHERE `username` = '$user' ";
 
         $result = $conn->query($query);
@@ -21,6 +40,24 @@ echo showHeader();
             }
             echo "</table>";
         } 
+        
+        
+    
+    
+      
+        
+        
+        
+       $sql="DELETE FROM kunstwerk WHERE id='$_GET[id]'";
+
+
+
+if(mysqli_query($con, $sql))
+
+        header("refresh:1; url=collection.php");  
+        
+        
+        
 
 echo showFooter();
 ?>
